97e29477fd20ea8ee3cfe1c79db8feda8099bc3c
Gaurav Dogra <gauru.dogra@gmail.com>
2021-04-06 22:49:39 +0100

    Removed code adding Users via CommandLineRunners

--- Web Quiz Engine/task/src/engine/WebQuizEngine.java
+++ Web Quiz Engine/task/src/engine/WebQuizEngine.java
@@ -12,17 +12,4 @@ public class WebQuizEngine {
     public static void main(String[] args) {
         SpringApplication.run(WebQuizEngine.class, args);
     }
-
-    @Bean
-    public CommandLineRunner demo(UserRepository repository) {
-        System.out.println("SpringsecurityjpaApplication.demo");
-        return (args) -> {
-            // save a few users
-            repository.save(new User("admin", "adminpass", true, "ROLE_USER,ROLE_ADMIN"));
-            repository.save(new User("user", "pass", true, "ROLE_USER"));
-            repository.findAll()
-                    .forEach(System.out::println);
-        };
-    }
-
 }
