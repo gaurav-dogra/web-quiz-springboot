774850a20d19c19ebfeb6d356848a71093148779
Gaurav Dogra <gauru.dogra@gmail.com>
2021-04-06 21:47:51 +0100

    added two entries to User entity via CommandLineRunner

--- Web Quiz Engine/task/src/engine/WebQuizEngine.java
+++ Web Quiz Engine/task/src/engine/WebQuizEngine.java
@@ -1,13 +1,28 @@
 package engine;
 
+import engine.model.User;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class WebQuizEngine {
 
     public static void main(String[] args) {
         SpringApplication.run(WebQuizEngine.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner demo(UserRepository repository) {
+        System.out.println("SpringsecurityjpaApplication.demo");
+        return (args) -> {
+            // save a few users
+            repository.save(new User("admin", "adminpass", true, "ROLE_USER,ROLE_ADMIN"));
+            repository.save(new User("user", "pass", true, "ROLE_USER"));
+            repository.findAll()
+                    .forEach(System.out::println);
+        };
     }
 
 }
