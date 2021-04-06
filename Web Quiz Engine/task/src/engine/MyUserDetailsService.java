fe1ba66a6e255041f5d066d3f46ee12ec57de7a6
Gaurav Dogra <gauru.dogra@gmail.com>
2021-04-06 21:38:35 +0100

    added MyUserDetailsService.java

---
+++ Web Quiz Engine/task/src/engine/MyUserDetailsService.java
@@ -0,0 +1,27 @@
+package engine;
+
+import engine.model.MyUserDetails;
+import engine.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+    @Autowired
+    public MyUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByUserName(userName);
+        user.orElseThrow(() -> new UsernameNotFoundException(userName + " not found."));
+        return user.map(MyUserDetails::new).get();
+    }
+}
